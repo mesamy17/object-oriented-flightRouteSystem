65deddc65cfe794fa1be0b22870a3280b28e36b8
Samarth Sawant <sawant.samarths@gmail.com>
2026-07-13 13:41:53 +0530

    implemented result manager in find routes

--- findRoutes.js
+++ findRoutes.js
@@ -1,14 +1,20 @@
 const RouteService = require("./helpers/dataChecker");
 const FlightManager = require("./helpers/FlightManager");
+const ResultManager = require("./helpers/ResultManager");
 
 const routeService = new RouteService();
 const flightManager = new FlightManager();
+const resultManager = new ResultManager();
 
 function findRoutes(source, destination, maxStops) {
   try {
     const routes = routeService.specifier(source, destination, maxStops);
 
     const result = flightManager.getRoutes(source, destination, maxStops);
+
+    let output = resultManager.format(result);
+
+    return output;
   } catch (error) {
     return "unable to find routes";
   }
