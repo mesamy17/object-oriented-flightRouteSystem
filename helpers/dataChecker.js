022ad83a3f4b7ba3db12754f8151278220fcf323
Samarth Sawant <sawant.samarths@gmail.com>
2026-07-13 12:34:26 +0530

    addded file to check input data

---
+++ helpers/dataChecker.js
@@ -0,0 +1,19 @@
+class check {
+  constructor() {}
+
+  specifier(source, destination, maxStops) {
+    if (typeof source != "string") {
+      throw new Error("Invalid source");
+    }
+
+    if (typeof destination != "string") {
+      throw new Error("Invalid destination");
+    }
+
+    if (maxStops < 0 || maxStops > 5) {
+      throw new Error("Invalid stops");
+    }
+  }
+}
+
+module.exports = check;
